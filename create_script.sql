f2df98931669f021850f190c9117aee41a243ad1
Jacqueline Chan <jacquelinechan2@hotmail.com>
2019-07-23 18:21:07 -0400

    inserted amentities data

--- create_script.sql
+++ create_script.sql
@@ -94,3 +94,26 @@ CREATE TABLE IF NOT EXISTS reviews(
     rating decimal(2,1),
     reviewType CHAR(1)
 );
+
+-- set up the amendities table
+INSERT INTO amendities (amendName, amendDescription)
+VALUES
+('Essentials', 'Towels, bed sheets, soap, toilet paper, and pillows'),
+('Wifi', 'Free or paid wifi available'),
+('Shampoo', 'Hair care product'),
+('Hair dryer', 'A hair care tool to dry out hair'),
+('Closet/drawers', 'Furniture for guests to put their clothes'),
+('TV', 'Cable television or TV monitor'),
+('Heat', 'A system that maintains a warm temperature'),
+('Air Conditioning', 'A system that maintains a cool temperature'),
+('Breakfast, coffee, tea', 'Complimentary items or paid'),
+('Desk/workspace', 'Furniture for guests to work on'),
+('Fireplace', 'Electronic or traditional wood burning'),
+('Iron', 'A tool to smooth out clothes'),
+('Private entrance', 'An entrance Guests will not need to share with anyone else'),
+('Smoke detector', 'Check your local laws, which may require a working smoke detector in every room'),
+('Carbon monoxide detector', 'Check your local laws, which may require a working carbon monoxide detector in every room'),
+('First aid kit', 'A collection of supplies and equipment that is used to give medical treatment'),
+('Fire extinguisher', 'This is highly recommended fire safety prevention measure'),
+('Lock on bedroom door', 'Private room which can be locked for safety and privacy')
+;
