349a4dbe5d20503aa656a2ccc740a87fbb42cce3
Jimmy <jimmypang8@gmail.com>
2019-07-24 20:29:31 -0400

    added more queries for searching for listings

--- c43_eclipse/src/Server/ListingQueries.java
+++ c43_eclipse/src/Server/ListingQueries.java
@@ -4,6 +4,7 @@ import Listings.Listing;
 
 import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ListingQueries {
     public static ArrayList<Listing> searchRunner(Connection c){
@@ -20,9 +21,15 @@ public class ListingQueries {
         return q;
     }
 
-    public static String searchByLocation(String postcode){
+    public static String searchByLocation(Double latitude, Double longitude){
+
         String q = String.format("SELECT * FROM listing WHERE postal_code LIKE %s", postcode);
         return q;
+    }
+
+    public static String getListingsByDateRange(Date from, Date to){
+        // get the availabilites first
+        String q = String.format("SELECT * FROM listing WHERE (DATE >= TO )");
     }
 
 }
